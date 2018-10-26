f586c2541bf3e8aa61968691be3accbfca00e47d
kcluniesa <kcluniesa@yahoo.com>
2018-10-26 01:37:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-  return 
+  return "NO, NOT SINCE 1938!"
+else 
     
 end
 # Whatever you say to grandma, she should respond with
